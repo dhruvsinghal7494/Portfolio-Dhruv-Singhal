fe9b0f338de1fe73982afa5e24529f0b3d9b8b8b
Dhruv <122340013+dhruvsinghal7494@users.noreply.github.com>
2024-01-26 18:56:25 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ module.exports = {
     'Dhruv is a Software Developer, who loves learning new things and helping tech beginners.',
   siteKeywords:
     'Dhruv Singhal, Dhruvsinghal027, Software Developer, Web Developer, Quant Enthusiast, javascript, Leetcode, Next., HPAIR',
-  siteUrl: 'https://roopshreeudaiwal.netlify.app/',
+  siteUrl: 'https://portfolio-dhruvsinghal.vercel.app/',
   siteLanguage: 'en_US',
   googleAnalyticsID: 'UA-45666519-2',
   googleVerification: 'DCl7VAf9tcz6eD9gb67NfkNnJ1PKRNcg8qQiwpbx9Lk',
